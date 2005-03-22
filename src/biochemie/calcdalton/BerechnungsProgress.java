cc78ec0e09046fa26d5d684879a9edc4247fe5cc
dienst <dienst>
2005-03-22 12:23:36 +0000

    maxclique, wenn keine vollstaendige loesung moeglich

--- src/biochemie/calcdalton/BerechnungsProgress.java
+++ src/biochemie/calcdalton/BerechnungsProgress.java
@@ -13,6 +13,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -29,9 +33,20 @@ import javax.swing.JTable;
 import javax.swing.Timer;
 import javax.swing.table.TableColumn;
 
+import org._3pq.jgrapht.UndirectedGraph;
+import org.apache.commons.functor.Algorithms;
+import org.apache.commons.functor.UnaryFunction;
+
 import biochemie.calcdalton.gui.CDConfig;
 import biochemie.calcdalton.gui.SBEGui;
 import biochemie.calcdalton.gui.SBEPanel;
+import biochemie.gui.TaskRunnerDialog;
+import biochemie.sbe.calculators.MaximumCliqueFinder;
+import biochemie.sbe.multiplex.Multiplexable;
+import biochemie.util.GraphHelper;
+import biochemie.util.GraphWriter;
+import biochemie.util.Helper;
+import biochemie.util.SwingWorker;
 
 public class BerechnungsProgress extends JFrame{
 	JFrame frame;
@@ -50,6 +65,7 @@ public class BerechnungsProgress extends JFrame{
 	public BerechnungsProgress() {
 		super("Berechnung läuft...");
         calcthread=Thread.currentThread();
+        calcthread.setName("CalcDaltonThread");
 		//this.sbe=SBEGui.getInstance();
         getContentPane().setLayout(new BorderLayout());
 		progress=new JProgressBar(0);
@@ -63,7 +79,7 @@ public class BerechnungsProgress extends JFrame{
         cancel.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
                 timer.stop();
-                calcthread.stop();
+                calcthread.interrupt();
                 dispose();
             }
         });
@@ -88,14 +104,109 @@ public class BerechnungsProgress extends JFrame{
        });
        start();
     }
-
+	private void showCDResultTable(final SBETable sbetable) {
+        JTableEx tabelle;
+        JScrollPane scrollPane;
+        final JFrame frame;
+        JButton jb_next;
+        JButton jb_prev;
+        JButton showDiffs;
+        frame = new JFrame("Result "+(sbetable.getIndex()+1)+" of "+sbetable.getNumberOfSolutions());
+        double p=TableLayoutConstants.PREFERRED;
+        double f=TableLayoutConstants.FILL;
+        double b=5;
+        double[][] sizes={{b,f,b},{b,f,3*b,p,b,25,b}};
+        frame.getContentPane().setLayout(new TableLayout(sizes));
+        tabelle = new JTableEx(sbetable);
+        tabelle.addKeyListener(new KeyAdapter(){//Eastereggs :-)
+            public void keyPressed(KeyEvent e) {
+                if(KeyEvent.VK_Q == e.getKeyCode()){
+                    final SwingWorker worker =new SwingWorker() {
+                        public Object construct() {
+                            return new TubeBlazerLight();
+                        }
+                    };
+                    worker.start();
+                }if(KeyEvent.VK_P == e.getKeyCode()){
+                    final SwingWorker worker =new SwingWorker() {
+                        public Object construct() {
+                            Pool.main(new String[0]);
+                            return null;
+                        }
+                    };
+                    worker.start();
+                }if(KeyEvent.VK_2 == e.getKeyCode()){
+                    final SwingWorker worker =new SwingWorker() {
+                        public Object construct() {
+                            Slime2P.main(new String[0]);
+                            return null;
+                        }
+                    };
+                    worker.start();
+                } 
+            }
+        });
+        scrollPane = new JScrollPane(tabelle);
+        tabelle.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        TableColumn column = null;
+        for (int i = 0; i < tabelle.getColumnModel().getColumnCount(); i++) {
+            column = tabelle.getColumnModel().getColumn(i);
+            int width=sbetable.getMaxLengthOfColumn(i);
+            if(width <= 130)
+                column.setPreferredWidth(width);
+            else{
+                column.setPreferredWidth(100);
+                
+            }
+        }
+        frame.getContentPane().add(scrollPane,"1,1");
+        JLabel label=new JLabel("<html>All molecular weights refer to the " +
+                                "3'residue after cleavage.<br>They include " +
+                                "the residual photolinker weight.</html>");
+        frame.getContentPane().add(label,"1,3");
+        JPanel buttonpanel=new JPanel();
+        buttonpanel.setLayout(new BoxLayout(buttonpanel,BoxLayout.X_AXIS));
+        jb_next=new JButton("Next result");
+        jb_next.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                sbetable.nextLoesung();
+                frame.setTitle("Result "+(sbetable.getIndex()+1)+" of "+sbetable.getNumberOfSolutions());
+                frame.repaint();
+            }
+        });
+        jb_prev=new JButton("Previous result");
+        jb_prev.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                sbetable.previousLoesung();
+                frame.setTitle("Result "+(sbetable.getIndex()+1)+" of "+sbetable.getNumberOfSolutions());
+                frame.repaint();
+            }
+        });
+        showDiffs=new JButton("Show mass differences");
+        showDiffs.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                JFrame f=new JFrame("Diffs for result "+(sbetable.getIndex()+1));
+                DiffTableModel dtm=new DiffTableModel(sbetable);
+                JTable t=new JTable(dtm);
+                JScrollPane sp=new JScrollPane(t);
+                f.getContentPane().add(sp);
+                f.pack();
+                f.setVisible(true);
+            }
+        });
+        buttonpanel.add(jb_prev);
+        buttonpanel.add(jb_next);
+        buttonpanel.add(Box.createGlue());
+        buttonpanel.add(showDiffs);
+        frame.getContentPane().add(buttonpanel,"1,5");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+        sbe.start.setEnabled(true);
+    }
 	public void start() {
-		JTableEx tabelle;
-		JScrollPane scrollPane;
-		final JFrame frame;
-		JButton jb_next;
-		JButton jb_prev;
-		JButton showDiffs;
+
 		
 		setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 		/**
@@ -159,110 +270,126 @@ public class BerechnungsProgress extends JFrame{
             starttime=System.currentTimeMillis();
         }
         timer.start();
-        cd.calc(paneldata,sbetable,fest);
+        //calcdalton veraendert das array, also muss ichs erst kopieren
+        int[] tempfest = new int[fest.length];
+        System.arraycopy(fest,0,tempfest,0,fest.length);
+        cd.calc(paneldata,sbetable,tempfest);
+        if(calcthread.isInterrupted())
+            return;
 		timer.stop();
         if(CalcDalton.debug){
             long endtime=System.currentTimeMillis();
 		    System.out.println("Dauer: "+(endtime-starttime)+"ms");
         }
 		if(0 == sbetable.getNumberOfSolutions()){
-			JOptionPane.showMessageDialog(null, "Sorry, just the first "+(cd.getMaxReachedDepth()+1)+" ones worked together.");
-			return;
-		}
-		frame = new JFrame("Result "+(sbetable.getIndex()+1)+" of "+sbetable.getNumberOfSolutions());
-        double p=TableLayoutConstants.PREFERRED;
-        double f=TableLayoutConstants.FILL;
-        double b=5;
-        double[][] sizes={{b,f,b},{b,f,3*b,p,b,25,b}};
-        frame.getContentPane().setLayout(new TableLayout(sizes));
-        tabelle = new JTableEx(sbetable);
-	    tabelle.addKeyListener(new KeyAdapter(){//Eastereggs :-)
-	        public void keyPressed(KeyEvent e) {
-	            if(KeyEvent.VK_Q == e.getKeyCode()){
-	                final SwingWorker worker =new SwingWorker() {
-	                    public Object construct() {
-	                        return new TubeBlazerLight();
-	                    }
-	                };
-	                worker.start();
-	            }if(KeyEvent.VK_P == e.getKeyCode()){
-	                final SwingWorker worker =new SwingWorker() {
-	                    public Object construct() {
-	                        Pool.main(new String[0]);
-	                        return null;
-	                    }
-	                };
-	                worker.start();
-	            }if(KeyEvent.VK_2 == e.getKeyCode()){
-	                final SwingWorker worker =new SwingWorker() {
-	                    public Object construct() {
-	                        Slime2P.main(new String[0]);
-	                        return null;
-	                    }
-	                };
-	                worker.start();
-	            } 
-	        }
-	    });
-		scrollPane = new JScrollPane(tabelle);
-        tabelle.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        TableColumn column = null;
-        for (int i = 0; i < tabelle.getColumnModel().getColumnCount(); i++) {
-            column = tabelle.getColumnModel().getColumn(i);
-            int width=sbetable.getMaxLengthColumn(i);
-            if(130 >= width)
-                column.setPreferredWidth(width);
-            else{
-                column.setPreferredWidth(100);
-                
-            }
+			int result=JOptionPane.showConfirmDialog(null, "Sorry, just the first "+(cd.getMaxReachedDepth()+1)+" ones worked together." +
+                    "Would you like to find the maximal subset of fitting primers?", "Enhanced calculation",JOptionPane.YES_NO_OPTION);
+			if(result == JOptionPane.NO_OPTION) {
+                return;
+            }else
+                findMaxClique(cd,SBENames,paneldata, fest,br);
+            
+		}else {
+		    showCDResultTable(sbetable);      
         }
-        frame.getContentPane().add(scrollPane,"1,1");
-        JLabel label=new JLabel("<html>All molecular weights refer to the " +
-                                "3'residue after cleavage.<br>They include " +
-                                "the residual photolinker weight.</html>");
-        frame.getContentPane().add(label,"1,3");
-        JPanel buttonpanel=new JPanel();
-        buttonpanel.setLayout(new BoxLayout(buttonpanel,BoxLayout.X_AXIS));
-        jb_next=new JButton("Next result");
-        jb_next.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                sbetable.nextLoesung();
-                frame.setTitle("Result "+(sbetable.getIndex()+1)+" of "+sbetable.getNumberOfSolutions());
-                frame.repaint();
-            }
-        });
-        jb_prev=new JButton("Previous result");
-        jb_prev.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                sbetable.previousLoesung();
-                frame.setTitle("Result "+(sbetable.getIndex()+1)+" of "+sbetable.getNumberOfSolutions());
-                frame.repaint();
-            }
-        });
-        showDiffs=new JButton("Show mass differences");
-        showDiffs.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				JFrame f=new JFrame("Diffs for result "+(sbetable.getIndex()+1));
-				DiffTableModel dtm=new DiffTableModel(SBENames,sbetable);
-				JTable t=new JTable(dtm);
-				JScrollPane sp=new JScrollPane(t);
-				f.getContentPane().add(sp);
-				f.pack();
-				f.setVisible(true);
-			}
-        });
-        buttonpanel.add(jb_prev);
-        buttonpanel.add(jb_next);
-        buttonpanel.add(Box.createGlue());
-        buttonpanel.add(showDiffs);
-		frame.getContentPane().add(buttonpanel,"1,5");
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.pack();
-        frame.setVisible(true);
-        setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		sbe.start.setEnabled(true);
+
 	}
+    /**
+     * @param paneldata
+     * @param fest
+     */
+    private void findMaxClique(final CalcDalton cd,String[] names,final String[][] paneldata, int[] fest, final int[] br) {
+        System.out.println("Using fest="+Helper.toString(fest));
+        List primer = new ArrayList(paneldata.length * br.length);
+        
+        for (int i = 0; i < paneldata.length; i++) {
+            if(fest[i]!= -1)
+                for (int j = 0; j < br.length; j++) {
+                    primer.add(new SimplePrimer(cd,names[i],paneldata[i],br[j]));
+                }
+            else
+                primer.add(new SimplePrimer(cd,names[i],paneldata[i],fest[i]));
+        }
+        final UndirectedGraph graph=GraphHelper.getKomplementaerGraph(GraphHelper.createIncompGraph(primer,true,GraphWriter.TGF));
+        final TaskRunnerDialog dialog = new TaskRunnerDialog("Searching for max. clique",null,new SwingWorker() {
+            public Object construct() {
+                MaximumCliqueFinder mcf = new MaximumCliqueFinder(graph,paneldata.length,true);
+                Set max= mcf.maxClique();
+                
+                String[] cliquenames=(String[]) Algorithms.collect(Algorithms.apply(max.iterator(), new UnaryFunction() {
+                    public Object evaluate(Object obj) {
+                        return ((SimplePrimer)obj).name;
+                    }
+                }), new ArrayList(max.size())).toArray(new String[max.size()]);
+                
+                SBETable sbet= new SBETable(cliquenames, br);
+                String[][] sbedata = new String[max.size()][];
+                int[] fest = new int[max.size()];
+                int i=0;
+                for (Iterator iter = max.iterator(); iter.hasNext();i++) {
+                    SimplePrimer primer = (SimplePrimer) iter.next();
+                    sbedata[i]=primer.datarow;
+                    fest[i]=primer.fest;
+                }
+                
+                cd.calc(sbedata,sbet,fest);
+                return sbet;
+            }
+            public void finished() {
+                SBETable table=(SBETable)getValue();
+                showCDResultTable(table);
+            }
+        });
+        dialog.show();
+
+    }
+    private class SimplePrimer implements Multiplexable{
+        private CalcDalton cd;
+        private String[] datarow;
+        int fest;
+        String plexid;
+        private String name;
+        
+        public SimplePrimer(CalcDalton cd, String name,String[] row, int f) {
+            this.cd=cd;
+            this.name=name;
+            this.datarow=row;
+            this.fest=f;
+        }
+
+        public String toString() {
+            return name+":"+datarow+"; "+fest;
+        }
+        public void setPlexID(String s) {
+            if(plexid !=null)
+                throw new IllegalStateException("Duplicate plexid!");
+            this.plexid=s;
+        }
+
+        public String getName() {
+            return datarow[0];
+        }
+
+        public boolean passtMit(Multiplexable other) {
+            String[][] sbedata= {datarow
+                                ,((SimplePrimer)other).datarow};
+            int[] fest=new int[] {this.fest, ((SimplePrimer)other).fest};
+            if(cd.calc(sbedata, fest).length == 0) {//keine Loesung
+                return false;
+            }
+            return true;
+        
+        }
+
+        public int maxPlexSize() {
+            return Integer.MAX_VALUE;
+        }
+
+        public String getEdgeReason() {
+            return "cd";
+        }
+
+    }
 
 }
 
