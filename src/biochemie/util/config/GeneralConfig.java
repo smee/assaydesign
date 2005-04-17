e00d06856d0448a74edd40044bb82217312dd891
dienst <dienst>
2005-04-17 10:55:43 +0000

    BUG: loeshcte alle Kommentare

--- src/biochemie/util/config/GeneralConfig.java
+++ src/biochemie/util/config/GeneralConfig.java
@@ -96,6 +96,7 @@ public abstract class GeneralConfig extends Observable{
                 } else {//kommentar oder unsinnige zeile, kein Parameter
                     //wird weggelassen, weil es ja eh n problem beim spaeteren einlesen geben wuerde.
                     //System.out.println("unknown parameterline: "+temp);
+                    sb.append(line + '\n');
                 }
             }
             in.close();
