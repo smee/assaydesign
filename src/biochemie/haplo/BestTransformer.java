195b6123dbc7fc48245457a52af9f648471600df
dienst <dienst>
2006-04-07 05:48:23 +0000

    *** empty log message ***

--- src/biochemie/haplo/BestTransformer.java
+++ src/biochemie/haplo/BestTransformer.java
@@ -7,9 +7,16 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class BestTransformer {
-
+    static interface LineProcessor{
+        String process(String line);
+    }
     public BestTransformer() {
         super();
         // TODO Auto-generated constructor stub
@@ -29,41 +36,53 @@ public class BestTransformer {
             System.err.println("File \""+args[0]+"\" doesn't exist.");
             System.exit(0);
         }
-        process(f);
+        final StringBuffer orig=new StringBuffer(); 
+        process(f,File.createTempFile("__best","tmp"),new LineProcessor() {
+            public String process(String line) {
+                if(orig.length()==0)
+                    orig.append(line);
+                else
+                    for(int i=0; i<line.length();i++)
+                        if(line.charAt(i)=='.')
+                            orig.setCharAt(i,'x');//mark for deletion
+                return line;
+            }
+            
+        });
+        String newf=f.getAbsolutePath()+".result.txt";
+        System.out.println("Writing results to \""+newf+"\"...");
+        process(f,new File(newf),new LineProcessor() {
+            public String process(String line) {
+                StringBuffer sb=new StringBuffer(line.length());
+                for(int i=0;i<line.length();i++) {
+                    if(orig.charAt(i)=='x')
+                        continue;
+                    sb.append(adjustChar(orig.charAt(i),line.charAt(i)));
+                }
+                return new String(sb);
+            }
+        });
         System.out.println("Done.");
     }
 
-    private static void process(File f) throws IOException {
+    private static void process(File f, File newf, LineProcessor proc) throws IOException {
         BufferedReader br=new BufferedReader(new FileReader(f));
-        String newfile=f.getAbsolutePath()+".txt";
-        System.out.println("Writing results to "+newfile+"...");
-        BufferedWriter bw=new BufferedWriter(new FileWriter(newfile));
-        
+        BufferedWriter bw=new BufferedWriter(new FileWriter(newf));
         try {
-            String line=br.readLine();
-            final String firstline=br.readLine();
-            bw.write(firstline);
+            String line=br.readLine();            
             while((line=br.readLine())!=null) {
                 if(line.length() == 0 || line.charAt(0)=='>')
                     continue;
-                bw.write(adjustLine(firstline, line));
+                bw.write(proc.process(line));
                 bw.write('\n');
             }
         }finally {
             bw.close();
-            br.close();
+            br.close();            
         }
     }
 
-    private static String adjustLine(String orig, String line) {
-        if(orig.length()!=line.length()) {
-            
-        }
-        StringBuffer res=new StringBuffer(line.length());
-        for(int i=0;i<orig.length();i++)
-            res.append(adjustChar(orig.charAt(i),line.charAt(i)));
-        return res.toString();
-    }
+
 
     private static String adjustChar(char orig, char c) {
         switch (c) {
@@ -74,7 +93,10 @@ public class BestTransformer {
         case '.'://deletion
             return "";
         default:
-            return Character.toString(c);
+            if(orig == '.')
+                return "";
+            else
+                return Character.toString(c);
         }
     }
 
